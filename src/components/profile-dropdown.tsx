1db507dc2b861e1d367ea753b6db4b99ecc99270
lskeey <muhammd.alviansyah@gmail.com>
2025-08-19 18:36:44 +0700

    feat: improve profile dropdown menu components

--- src/components/profile-dropdown.tsx
+++ src/components/profile-dropdown.tsx
@@ -12,11 +12,14 @@ import {
   DropdownMenuSubContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { EllipsisVertical, Check } from "lucide-react";
+import { EllipsisVertical, Check, LogOut } from "lucide-react";
 import { useTheme } from "next-themes";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { getInitials } from "@/lib/utils";
 
 export function ProfileDropdown() {
   const { theme, setTheme } = useTheme();
+  const username = "John Doe";
 
   return (
     <DropdownMenu>
@@ -26,26 +29,51 @@ export function ProfileDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="start">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+        <DropdownMenuLabel className="p-0 font-normal">
+          <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+            <Avatar className="h-8 w-8 rounded-lg">
+              {/* <AvatarImage src={user.avatar} alt={user.name} /> */}
+              <AvatarFallback className="rounded-lg">
+                {getInitials(username)}
+              </AvatarFallback>
+            </Avatar>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-medium">{username}</span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer">
+            Profile
+          </DropdownMenuItem>
           <DropdownMenuSub>
             <DropdownMenuSubTrigger>Theme</DropdownMenuSubTrigger>
             <DropdownMenuPortal>
               <DropdownMenuSubContent>
-                <DropdownMenuItem onClick={() => setTheme("light")}>
+                <DropdownMenuItem
+                  onClick={() => setTheme("light")}
+                  className="cursor-pointer"
+                >
                   <span className="flex items-center justify-between w-full">
                     Light
                     {theme === "light" && <Check className="h-4 w-4" />}
                   </span>
                 </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                <DropdownMenuItem
+                  onClick={() => setTheme("dark")}
+                  className="cursor-pointer"
+                >
                   <span className="flex items-center justify-between w-full">
                     Dark
                     {theme === "dark" && <Check className="h-4 w-4" />}
                   </span>
                 </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
+                <DropdownMenuItem
+                  onClick={() => setTheme("system")}
+                  className="cursor-pointer"
+                >
                   <span className="flex items-center justify-between w-full">
                     System
                     {theme === "system" && <Check className="h-4 w-4" />}
@@ -56,7 +84,13 @@ export function ProfileDropdown() {
           </DropdownMenuSub>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Log out</DropdownMenuItem>
+        <DropdownMenuItem
+          className="cursor-pointer flex justify-between items-center"
+          variant="destructive"
+        >
+          Log out
+          <LogOut />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
