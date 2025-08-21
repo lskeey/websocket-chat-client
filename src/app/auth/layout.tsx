17939f545b90ea8a8c6524e3ff2b8f54d7ae8df0
lskeey <muhammd.alviansyah@gmail.com>
2025-08-21 12:03:13 +0700

    chore: update image imports to next/legacy/image

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 export default function AuthCardLayout({
   children,
